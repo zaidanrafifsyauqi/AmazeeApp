96154763a193a81036c6c10ccc80ae93edbe96e0
zaidan <zaidanrafifsyauqi28@gmail.com>
2023-12-21 11:02:35 +0700

    update dan meperbaiki fitur

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.github.denzcoskun:ImageSlideshow:0.1.2")
 
 
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,9 +10,11 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven(url = "https://jitpack.io")
     }
 }
 
 rootProject.name = "AmikomChat"
 include(":app")
+
  
